50b653a9fcc89e61f65db5f4aeae1f1e2e0bd5ad
Leonid Barsht <leonidb93@gmail.com>
2023-11-25 14:26:04 +0100

    implement basic movement

---
+++ Assets/Scripts/PlayerMover.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMover : MonoBehaviour
+{
+    public CharacterController CharacterController;
+    public float MoveDuration = 1;
+
+    private bool moving;
+
+    private void OnMove(InputValue inputValue)
+    {
+        if (moving) return;
+
+        Vector2 move = inputValue.Get<Vector2>();
+        Vector3 movement = Vector3.zero;
+
+        if (move.x != 0)
+            movement.x = move.x;
+        else if (move.y != 0)
+            movement.z = move.y;
+
+        StartCoroutine(MovePlayer(movement));
+    }
+
+    IEnumerator MovePlayer(Vector3 movement)
+    {
+        moving = true;
+        float elapsed = 0;
+
+        Vector3 startPosition = transform.position;
+        Vector3 endPosition = startPosition + movement;
+
+        while (elapsed < MoveDuration)
+        {
+            transform.position = Vector3.Lerp(startPosition, endPosition, elapsed / MoveDuration);
+            elapsed += Time.deltaTime;
+            yield return null;
+        }
+
+        transform.position = endPosition;
+
+        moving = false;
+    }
+}
